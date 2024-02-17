cabd3517ad9e20ef447d87f6fe10a507ae9442e2
dhruvi188 <dhruvigohel2312@gmail.com>
2024-02-17 23:49:18 +0530

    post

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
 
@@ -9,6 +11,24 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+class Blog(BaseModel):
+    title: str
+    body: str
+    description: str | None = None
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    title = request.title
+    return {'data': f'{request.title}'}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {'data': 'unpublished'}
+
+@app.get("/blog/{id}")
+def blogs(id: int):
+    return {'data': id}
